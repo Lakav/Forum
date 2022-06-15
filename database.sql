a6b9158317748680095889f0083ccf09f313ee4f
julescavanier <jules.cavanier@ynov.com>
2022-06-15 06:23:19 +0200

    add html

--- database.sql
+++
@@ -1,6 +0,0 @@
-    CREATE TABLE `userinfo` (
-        `uid` INTEGER PRIMARY KEY AUTOINCREMENT,
-        `username` VARCHAR(64) NULL,
-        `departname` VARCHAR(64) NULL,
-        `created` DATE NULL
-    );
