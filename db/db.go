d69c1d0102b34ef4a41a530d595113228eaaf09e
julescavanier <jules.cavanier@ynov.com>
2022-06-16 13:58:49 +0200

    update

--- db/db.go
+++ db/db.go
@@ -2,18 +2,14 @@ package db
 
 import (
 	"database/sql"
-	// "fmt"
+
 	"log"
-	// "os"
 )
 
 var DB *sql.DB
 
 func Init() {
-	// err := os.WriteFile("filename.txt", []byte("Hello"), 0755)
-	// if err != nil {
-	// 	fmt.Printf("Unable to write file: %v", err)
-	// }
+
 	database, connectionErr := sql.Open("sqlite3", "database.db")
 	if connectionErr != nil {
 		log.Fatal(connectionErr)
